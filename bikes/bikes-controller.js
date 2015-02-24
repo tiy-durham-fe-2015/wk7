38741cb3356227c6f1ff4147959c902e38a6c655
Chris Davies <chris.davies@theironyard.com>
2015-02-24 11:50:38 -0500

    Commenting the bikes page

--- bikes/bikes-controller.js
+++ bikes/bikes-controller.js
@@ -1,23 +1,31 @@
 angular.module('app', []).controller('BikesCtrl', function () {
   var self = this;
 
+  // Indicates whether or not we are showing the promotional view
   self.showPromos = true;
 
+  // Our collection of bikes
   self.bikes = [];
 
+  // Add a bike to our bikes list
   self.addBike = function () {
     self.bikes.push(Bike());
   };
 
+  // We enable editing by setting showPromos to false
   self.editBikes = function (edit) {
     self.showPromos = !edit;
   };
 
+  // Construct a new bike instance
   function Bike(spec) {
+    // A bike is a product
     var self = Product(spec);
 
+    // A bike has accessories
     self.accessories = [];
 
+    // We can add accessories to a bike by calling bike.addAccessory()
     self.addAccessory = function () {
       self.accessories.push(Product());
     };
@@ -25,10 +33,14 @@ angular.module('app', []).controller('BikesCtrl', function () {
     return self;
   }
 
+  // Construct a new product instance
   function Product(spec) {
+    // If spec was not passed, initialize it to an empty object
     spec = spec || {};
     return {
+      // If name was not passed, give it some default ('Unknown product')
       name: spec.name || 'Unknown product',
+      // If price was not passed, give it some default (0)
       price: spec.price || 0
     };
   }
